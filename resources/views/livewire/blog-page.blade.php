11044567626761ec3aca8f8cb9c8dc237aedef3f
Salman Developer <salmandeveloper@example.com>
2025-04-25 10:39:53 +0500

    first commit

--- resources/views/livewire/blog-page.blade.php
+++ resources/views/livewire/blog-page.blade.php
@@ -1,6 +1,6 @@
 <div>
 	<!-- start banner Area -->
-	<section class="banner-area relative bg-warning blog-home-banner" id="home">
+	<section class="banner-area relative bg-primary blog-home-banner" id="home">
 		<div class="overlay overlay-bg"></div>
 		<div class="container">
 			<div class="row d-flex align-items-center justify-content-center">
