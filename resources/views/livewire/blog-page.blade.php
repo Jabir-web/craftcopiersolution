fbe1361546d0b12d9f01c92da0e47d93c9169aef
Salman Developer <salmandeveloper@example.com>
2025-04-25 10:20:56 +0500

    first commit

--- resources/views/livewire/blog-page.blade.php
+++ resources/views/livewire/blog-page.blade.php
@@ -102,7 +102,7 @@
 										<img src="{{ url('storage/' . $blog->img) }}" alt="{{ $blog->title }}" class="img-fluid fixed-image">
 									</div>
 									<a class="posts-title" href="blog-single.html"><h3>{{ $blog->title }}</h3></a>
-									<p class="excert">
+									<p class="text-info">
 										{{ $blog->description }}
 									</p>
 									<a href="blog-single.html" class="primary-btn">View More</a>
