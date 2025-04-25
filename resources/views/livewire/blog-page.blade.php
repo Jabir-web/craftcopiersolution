58edcb97f14f46dc7089fddfc78100856db045d9
Salman Developer <salmandeveloper@example.com>
2025-04-25 10:36:25 +0500

    first commit

--- resources/views/livewire/blog-page.blade.php
+++ resources/views/livewire/blog-page.blade.php
@@ -1,6 +1,6 @@
 <div>
 	<!-- start banner Area -->
-	<section class="banner-area relative blog-home-banner" id="home">
+	<section class="banner-area relative bg-warning blog-home-banner" id="home">
 		<div class="overlay overlay-bg"></div>
 		<div class="container">
 			<div class="row d-flex align-items-center justify-content-center">
@@ -192,38 +192,14 @@
 									<li>
 										<a href="#" class="d-flex justify-content-between">
 											<p>{{ $blog->category }}</p> <!-- Displaying category name -->
-										
+
 										</a>
 									</li>
 								@endforeach
 							</ul>
 						</div>
-						
-						<div class="single-sidebar-widget newsletter-widget">
-							<h4 class="newsletter-title">Newsletter</h4>
-							<p>
-								Here, I focus on a range of items and features that we use in life without
-								giving them a second thought.
-							</p>
-							<div class="form-group d-flex flex-row">
-								<div class="col-autos">
-									<div class="input-group">
-										<div class="input-group-prepend">
-											<div class="input-group-text"><i class="fa fa-envelope"
-													aria-hidden="true"></i>
-											</div>
-										</div>
-										<input type="text" class="form-control" id="inlineFormInputGroup"
-											placeholder="Enter email" onfocus="this.placeholder = ''"
-											onblur="this.placeholder = 'Enter email'">
-									</div>
-								</div>
-								<a href="#" class="bbtns">Subcribe</a>
-							</div>
-							<p class="text-bottom">
-								You can unsubscribe at any time
-							</p>
-						</div>
+
+
 						<div class="single-sidebar-widget tag-cloud-widget">
 							<h4 class="tagcloud-title">Tag Clouds</h4>
 							<ul>
