b885d648a7cf3401f596bbd6e19a1cac43bafab2
Salman Developer <salmandeveloper@example.com>
2025-04-27 09:47:49 +0500

    first commit

--- resources/views/livewire/machine-page.blade.php
+++ resources/views/livewire/machine-page.blade.php
@@ -37,7 +37,7 @@
                                             </div>
                                             <div class="col-md-8 d-flex flex-column justify-content-center p-4">
                                                 <h3 class="card-title mb-3" style="font-size: 1.8rem;">{{ $machine->name }}</h3>
-                                                <p class="card-text" style="color: #555;">{{ \Illuminate\Support\Str::limit($machine->short_description, 300, '...') }}</p>
+                                                <p class="card-text" style="color: #555;">{{ \Illuminate\Support\Str::limit($machine->short_description, 500, '...') }}</p>
                                                 <div class="mt-3">
                                                     <a href="{{ $machine->brochure_link }}" class="btn btn-primary me-2" target="_blank">View Brochure</a>
                                                     <a href="{{ $machine->driver_link }}" class="btn btn-outline-secondary" target="_blank">Download Driver</a>
