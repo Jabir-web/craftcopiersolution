a30367dfc31e010bb475ff9e1ec228c6782f58cf
Salman Developer <salmandeveloper@example.com>
2025-05-04 08:02:22 +0500

    first commit

--- resources/views/livewire/blog-list.blade.php
+++ resources/views/livewire/blog-list.blade.php
@@ -5,39 +5,41 @@
                 <div class="menu-content pb-60 col-lg-9">
                     <div class="title text-center">
                         <h2 class="mb-10">Latest from Our Blog</h2>
-                        <p>With the exception of Nietzsche, no other madman has contributed so much to human sanity as has.</p>
+                        <p>With the exception of Nietzsche, no other madman has contributed so much to human sanity as
+                            has.</p>
                     </div>
                 </div>
             </div>
             <div class="row">
                 <div class="active-recent-blog-carusel">
                     @foreach($blogs as $blog)
-                    <div class="single-recent-blog-post item">
-                        <div class="thumb">
-                            <img src="{{ url('storage/' . $blog->img) }}" alt="{{ $blog->title }}" class="img-fluid fixed-image">
-                        </div>
-                        <div class="details">
-                            <div class="tags">
-                                <ul>
-                                    <li>
-                                        <a href="">{{ $blog->category }}</a>
-                                    </li>
-                                </ul>
+                        <div class="single-recent-blog-post item">
+                            <div class="thumb">
+                                <img src="{{ url('storage/' . $blog->img) }}" alt="{{ $blog->title }}"
+                                    class="img-fluid fixed-image">
                             </div>
-                
-                            <a href="{{ route('blogpage', ['title' => $blog->title]) }}">
-                                <h4 class="title">{{ $blog->title }}</h4>
-                            </a>
-                            <div class="blog-description mt-4"
-                            style="line-height: 1.8; color: #475569; font-size: 1.05rem;">
-                        {{  $blog->description }}
-                        </div>
+                            <div class="details">
+                                <div class="tags">
+                                    <ul>
+                                        <li>
+                                            <a href="">{{ $blog->category }}</a>
+                                        </li>
+                                    </ul>
+                                </div>
 
-                            <h6 class="date">{{ $blog->created_at->format('F j, Y') }}</h6>
+                                <a href="{{ route('blogpage', ['title' => $blog->title]) }}">
+                                    <h4 class="title">{{ $blog->title }}</h4>
+                                </a>
+                                {{-- <div class="blog-description mt-4"
+                                    style="line-height: 1.8; color: #475569; font-size: 1.05rem;">
+                                    {{  $blog->description }}
+                                </div> --}}
+
+                                <h6 class="date">{{ $blog->created_at->format('F j, Y') }}</h6>
+                            </div>
                         </div>
-                    </div>
-                @endforeach
-                
+                    @endforeach
+
                 </div>
             </div>
         </div>
