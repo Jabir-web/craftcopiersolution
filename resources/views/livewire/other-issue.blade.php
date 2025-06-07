cc6199b836046d9e528165e068a1b0d25bfe97cc
Salman <krosub0@gmail.com>
2025-06-07 21:19:44 +0500

    seo works

--- resources/views/livewire/other-issue.blade.php
+++ resources/views/livewire/other-issue.blade.php
@@ -1,69 +1,69 @@
 <div>
      <!-- Start issue Area -->
-     <section class="price-area section-gap">
+     <section class="price-area section-gap" aria-labelledby="other-issues-heading">
         <div class="container">
             <div class="row d-flex justify-content-center">
                 <div class="menu-content pb-70 col-lg-8">
                     <div class="title text-center">
-                        <h2 class="mb-10">Other issues we can help you with</h2>
-                        <p>Well educated, intellectual people, especially scientists at all times demonstrate
-                            considerably.</p>
+                        <h2 id="other-issues-heading" class="mb-10">Other issues we can help you with</h2>
+                        <p>Discover our range of specialized printing and copier services designed to meet your business needs efficiently and affordably.</p>
                     </div>
                 </div>
             </div>
             <div class="row">
+                <!-- Bulk Printing Service -->
                 <div class="col-lg-3 col-md-6">
                     <div class="single-other-issue">
                         <div class="thumb">
-                            <img class="img-fluid border rounded" src="{{ asset('front/img/bulk.png') }}" alt="bulk">
-
-
+                            <img class="img-fluid border rounded" src="{{ asset('front/img/bulk.png') }}" alt="Bulk Printing Service">
                         </div>
                         <a href="#">
                             <h4>Bulk Printing Service</h4>
                         </a>
                         <p>
-                            The preservation of human life is the ultimate value, a pillar of ethics and the foundation.
+                            We offer fast and reliable bulk printing solutions for businesses, events, and organizations. Get high-quality prints at competitive rates for all your large-volume needs.
                         </p>
                     </div>
                 </div>
+                <!-- Copier Repair Service -->
                 <div class="col-lg-3 col-md-6">
                     <div class="single-other-issue">
                         <div class="thumb">
-                            <img class="img-fluid border rounded" src="{{ asset('front/img/repair.png') }}" alt="repair">
+                            <img class="img-fluid border rounded" src="{{ asset('front/img/repair.png') }}" alt="Copier Repair Service">
                         </div>
                         <a href="#">
                             <h4>Copier Repair Service</h4>
                         </a>
                         <p>
-                            I was always somebody who felt quite sorry for myself, what I had not got compared.
+                            Our expert technicians provide prompt and professional repair services for all major copier brands, ensuring minimal downtime and maximum productivity for your business.
                         </p>
                     </div>
                 </div>
+                <!-- Photocopier Rentals -->
                 <div class="col-lg-3 col-md-6">
                     <div class="single-other-issue">
                         <div class="thumb">
-                            <img class="img-fluid border rounded" src="{{ asset('front/img/rental.png') }}" alt="rental">
+                            <img class="img-fluid border rounded" src="{{ asset('front/img/rental.png') }}" alt="Photocopier Rentals">
                         </div>
                         <a href="#">
                             <h4>Photocopier Rentals</h4>
                         </a>
                         <p>
-                            The following article covers a topic that has recently moved to center stage–at least it
-                            seems.
+                            Rent top-quality photocopiers for short or long-term use. Flexible rental plans and a wide selection of models to suit your office requirements and budget.
                         </p>
                     </div>
                 </div>
+                <!-- Get Copier On Per Copy Cost -->
                 <div class="col-lg-3 col-md-6">
                     <div class="single-other-issue">
                         <div class="thumb">
-                            <img class="img-fluid border rounded" src="{{ asset('front/img/percopy.png') }}" alt="percopy">
+                            <img class="img-fluid border rounded" src="{{ asset('front/img/percopy.png') }}" alt="Get Copier On Per Copy Cost">
                         </div>
                         <a href="#">
                             <h4>Get Copier On Per Copy Cost</h4>
                         </a>
                         <p>
-                            There are many kinds of narratives and organizing principles. Science is driven by evidence.
+                            Enjoy hassle-free copier usage with our per-copy cost plans. Pay only for what you print, with maintenance and support included for complete peace of mind.
                         </p>
                     </div>
                 </div>
