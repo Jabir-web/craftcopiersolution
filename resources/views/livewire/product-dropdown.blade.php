6672a235f285531300074b3332e4a132a4c98ff6
Salman <krosub0@gmail.com>
2025-06-07 21:21:27 +0500

    seo works

--- resources/views/livewire/product-dropdown.blade.php
+++ resources/views/livewire/product-dropdown.blade.php
@@ -1,7 +1,15 @@
-<li class="menu-has-children"><a href="#">Products</a>
+<li class="menu-has-children">
+    <a href="#" aria-haspopup="true" aria-expanded="false">Products</a>
     <ul>
         @foreach($products as $product)
-        <li><a wire:navigate href="{{ route('machines', ['product_id' => $product->id]) }}">{{ $product->name }}</a></li>
+        <li>
+            <a wire:navigate 
+               href="{{ route('machines', ['product_id' => $product->id]) }}"
+               title="View {{ $product->name }} machines"
+               aria-label="View {{ $product->name }} machines">
+                {{ $product->name }}
+            </a>
+        </li>
         @endforeach
     </ul>
 </li>
