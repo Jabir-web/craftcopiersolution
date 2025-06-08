5c27fe70418911db74b1c0f70cd7d98c8dd9c053
Salman <krosub0@gmail.com>
2025-06-08 15:29:45 +0500

    crack donwe

--- resources/views/livewire/contact-page.blade.php
+++ resources/views/livewire/contact-page.blade.php
@@ -24,7 +24,7 @@
     <section class="contact-page-area section-gap">
         <div class="container">
             <div class="row">
-                <div class="map-wrap" style="width:100%; height: 445px;">
+                <div class="map-wrap" style="width:50%; height: 445px;">
                     <iframe
                         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d462117.05296393496!2d66.82581529773248!3d25.193458567990753!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3eb33e06651d4bbf%3A0x9cf92f44555a0c23!2sKarachi%2C%20Pakistan!5e0!3m2!1sen!2s!4v1740514341752!5m2!1sen!2s"
                         width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy"
