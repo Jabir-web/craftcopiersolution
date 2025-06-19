35299890b2ed9294db82b429f394e834cb8daec8
Jabir Faisal <jabirmemon53@gmail.com>
2025-06-19 15:47:12 -0700

    Update BlogPage.php

--- app/Livewire/BlogPage.php
+++ app/Livewire/BlogPage.php
@@ -10,15 +10,16 @@ class BlogPage extends Component
     public $blogs; // All blogs for category list
     public $blog;  // Single blog
 
-    public function mount($title = null)
-    {
-        $this->blogs = Blog::all(); // Always load all blogs for sidebar/category/title list
+  public function mount($title = null)
+{
+    $this->blogs = Blog::all(); // Always load all blogs for sidebar/category/title list
 
-        if ($title) {
-            $this->blog = Blog::where('title', $title)->firstOrFail();
-        }
+    if ($title) {
+        $this->blog = Blog::where('title', $title)->firstOrFail();
+        // Increment the views column
+        $this->blog->increment('views');
     }
-
+}
     public function render()
     {
         return view('livewire.blog-page');
