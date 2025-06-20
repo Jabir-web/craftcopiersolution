403e2a92c6f73fd33c83516d3866c47959042fa1
Jabir Faisal <jabirmemon53@gmail.com>
2025-06-19 19:49:06 -0700

    Update blog-list.blade.php

--- resources/views/livewire/blog-list.blade.php
+++ resources/views/livewire/blog-list.blade.php
@@ -65,89 +65,61 @@
                             </div>
 
 
-                                               <div class="details">
-                            <div class="tags">
-                                <ul>
-                                    <li>
-                                        <a href="#" rel="tag" itemprop="about">{{ $blog->category }}</a>
-                                    </li>
-                                </ul>
+                            <div class="details">
+                                <div class="tags">
+                                    <ul>
+                                        <li>
+                                            <a href="#" rel="tag" itemprop="about">{{ $blog->category }}</a>
+                                        </li>
+                                    </ul>
+                                </div>
+                                <a href="{{ route('blogpage', ['title' => $blog->title]) }}">
+                                    <h2 class="title" itemprop="headline">{{ $blog->title }}</h2>
+                                </a>
+                                <meta itemprop="mainEntityOfPage"
+                                    content="{{ route('blogpage', ['title' => $blog->title]) }}">
+                                <meta itemprop="datePublished" content="{{ $blog->created_at->toIso8601String() }}">
+                                <meta itemprop="dateModified" content="{{ $blog->updated_at->toIso8601String() }}">
+                                <meta itemprop="author" content="Craft Copier Solution">
+                                <meta itemprop="publisher" content="Craft Copier Solution">
+                                <p class="blog-excerpt" itemprop="description">
+                                    {{ Str::limit(strip_tags($blog->description), 120) }}
+                                </p>
+                                <p class="mb-0 btn-style">
+                                    <i class="fas fa-eye"></i>
+                                    <strong>Views:</strong>
+                                    <span>{{ $blog->views }}</span>
+                                </p>
+                                <h6 class="date" itemprop="datePublished">{{ $blog->created_at->format('F j, Y') }}</h6>
+                                <!-- Share Buttons -->
+                                <div class="mt-2 mb-1 text-center">
+                                    <span class="fw-bold me-2" style="font-size:0.97em;">Share:</span>
+                                    <a href="https://www.facebook.com/sharer/sharer.php?u={{ urlencode(route('blogpage', ['title' => $blog->title])) }}"
+                                        target="_blank" rel="noopener" class="btn btn-sm btn-primary mx-1"
+                                        title="Share on Facebook">
+                                        <i class="fa fa-facebook"></i>
+                                    </a>
+                                    <a href="https://twitter.com/intent/tweet?url={{ urlencode(route('blogpage', ['title' => $blog->title])) }}&text={{ urlencode($blog->title) }}"
+                                        target="_blank" rel="noopener" class="btn btn-sm btn-info mx-1"
+                                        title="Share on Twitter">
+                                        <i class="fa fa-twitter"></i>
+                                    </a>
+                                    <a href="https://wa.me/?text={{ urlencode($blog->title . ' ' . route('blogpage', ['title' => $blog->title])) }}"
+                                        target="_blank" rel="noopener" class="btn btn-sm btn-success mx-1"
+                                        title="Share on WhatsApp">
+                                        <i class="fa fa-whatsapp"></i>
+                                    </a>
+                                    <a href="https://www.linkedin.com/shareArticle?mini=true&url={{ urlencode(route('blogpage', ['title' => $blog->title])) }}&title={{ urlencode($blog->title) }}"
+                                        target="_blank" rel="noopener" class="btn btn-sm btn-secondary mx-1"
+                                        title="Share on LinkedIn">
+                                        <i class="fa fa-linkedin"></i>
+                                    </a>
+                                    <a href="mailto:?subject={{ urlencode($blog->title) }}&body={{ urlencode(route('blogpage', ['title' => $blog->title])) }}"
+                                        class="btn btn-sm btn-dark mx-1" title="Share via Email">
+                                        <i class="fa fa-envelope"></i>
+                                    </a>
+                                </div>
                             </div>
-                            <a href="{{ route('blogpage', ['title' => $blog->title]) }}">
-                                <h2 class="title" itemprop="headline">{{ $blog->title }}</h2>
-                            </a>
-                            <meta itemprop="mainEntityOfPage"
-                                content="{{ route('blogpage', ['title' => $blog->title]) }}">
-                            <meta itemprop="datePublished" content="{{ $blog->created_at->toIso8601String() }}">
-                            <meta itemprop="dateModified" content="{{ $blog->updated_at->toIso8601String() }}">
-                            <meta itemprop="author" content="Craft Copier Solution">
-                            <meta itemprop="publisher" content="Craft Copier Solution">
-                            <p class="blog-excerpt" itemprop="description">
-                                {{ Str::limit(strip_tags($blog->description), 120) }}
-                            </p>
-                            <p class="mb-0 btn-style">
-                                <i class="fas fa-eye"></i>
-                                <strong>Views:</strong>
-                                <span>{{ $blog->views }}</span>
-                            </p>
-                            <h6 class="date" itemprop="datePublished">{{ $blog->created_at->format('F j, Y') }}</h6>
-                            <!-- Share Buttons -->
-                            <div class="mt-2 mb-1 text-center">
-                                <span class="fw-bold me-2" style="font-size:0.97em;">Share:</span>
-                                <a href="https://www.facebook.com/sharer/sharer.php?u={{ urlencode(route('blogpage', ['title' => $blog->title])) }}"
-                                    target="_blank" rel="noopener" class="btn btn-sm btn-primary mx-1"
-                                    title="Share on Facebook">
-                                    <i class="fa fa-facebook"></i>
-                                </a>
-                                <a href="https://twitter.com/intent/tweet?url={{ urlencode(route('blogpage', ['title' => $blog->title])) }}&text={{ urlencode($blog->title) }}"
-                                    target="_blank" rel="noopener" class="btn btn-sm btn-info mx-1"
-                                    title="Share on Twitter">
-                                    <i class="fa fa-twitter"></i>
-                                </a>
-                                <a href="https://wa.me/?text={{ urlencode($blog->title . ' ' . route('blogpage', ['title' => $blog->title])) }}"
-                                    target="_blank" rel="noopener" class="btn btn-sm btn-success mx-1"
-                                    title="Share on WhatsApp">
-                                    <i class="fa fa-whatsapp"></i>
-                                </a>
-                                <a href="https://www.linkedin.com/shareArticle?mini=true&url={{ urlencode(route('blogpage', ['title' => $blog->title])) }}&title={{ urlencode($blog->title) }}"
-                                    target="_blank" rel="noopener" class="btn btn-sm btn-secondary mx-1"
-                                    title="Share on LinkedIn">
-                                    <i class="fa fa-linkedin"></i>
-                                </a>
-                                <a href="mailto:?subject={{ urlencode($blog->title) }}&body={{ urlencode(route('blogpage', ['title' => $blog->title])) }}"
-                                    class="btn btn-sm btn-dark mx-1" title="Share via Email">
-                                    <i class="fa fa-envelope"></i>
-                                </a>
-                            </div>
-                        </div>
-                        <script type="application/ld+json">
-                            {
-                              "@context": "https://schema.org",
-                              "@type": "BlogPosting",
-                              "headline": "{{ $blog->title }}",
-                              "image": "{{ url('storage/' . $blog->img) }}",
-                              "author": {
-                                "@type": "Organization",
-                                "name": "Craft Copier Solution"
-                              },
-                              "publisher": {
-                                "@type": "Organization",
-                                "name": "Craft Copier Solution",
-                                "logo": {
-                                  "@type": "ImageObject",
-                                  "url": "{{ asset('front/img/favicon-32x32.png') }}"
-                                }
-                              },
-                              "datePublished": "{{ $blog->created_at->toIso8601String() }}",
-                              "dateModified": "{{ $blog->updated_at->toIso8601String() }}",
-                              "description": "{{ Str::limit(strip_tags($blog->description), 120) }}",
-                              "mainEntityOfPage": "{{ route('blogpage', ['title' => $blog->title]) }}"
-                            }
-                            </script>
-                    </article>
-                @endforeach
-
-                fix the arrangemtn of view area proffsinaly
                             <script type="application/ld+json">
                                 {
                                   "@context": "https://schema.org",
